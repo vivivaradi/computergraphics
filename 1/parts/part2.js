b66211e54676ce171ddd61939fe1b68fa81177b0
Vivien Varadi <vivien@shapegames.com>
2023-09-20 10:52:03 +0200

    finish part 2

--- 1/parts/part2.js
+++ 1/parts/part2.js
@@ -1,1 +1,23 @@
-window.onload = part1.init
+function init() {
+    canvas = document.getElementById('gl-canvas');
+    var gl = WebGLUtils.setupWebGL(canvas);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    program = initShaders(gl, "vertex-shader", "fragment-shader")
+    gl.useProgram(program)
+
+    var vertices = [ vec2(0.0, 0.0), vec2(1.0, 0.0), vec2(1.0, 1.0) ];
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition")
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(vPosition)
+
+    gl.drawArrays(gl.POINTS, 0, vertices.length)
+}
+
+window.onload = init
