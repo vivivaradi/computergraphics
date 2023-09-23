b5ad677e5ea4697ecd4eb3f108a6ae174d3a62e8
Vivien Varadi <vivien@shapegames.com>
2023-09-23 14:24:20 +0200

    finish part 4

--- 1/parts/part4.js
+++ 1/parts/part4.js
@@ -8,9 +8,7 @@ function init() {
     program = initShaders(gl, "vertex-shader", "fragment-shader")
     gl.useProgram(program)
 
-    var vertices = [ vec2(0.0, 0.0), vec2(1.0, 0.0), vec2(1.0, 1.0),
-                    vec2(0.0, 0.0), vec2(1.0, 1.0), vec2(0.0, 1.0)
-                    ];
+    var vertices = [ vec2(-0.5, -0.5), vec2(-0.5, 0.5), vec2(0.5, -0.5), vec2(0.5, 0.5) ];
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
@@ -19,7 +17,21 @@ function init() {
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0)
     gl.enableVertexAttribArray(vPosition)
 
-    gl.drawArrays(gl.TRIANGLES, 0, vertices.length)
+    var theta = 0.7854
+    var thetaLoc = gl.getUniformLocation(program, "theta")
+
+    function render()
+    {
+        setTimeout(function() {
+            requestAnimFrame(render);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+            theta += 0.1;
+            gl.uniform1f(thetaLoc, theta);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        }, 100);
+    }
+
+    render()
 }
 
 window.onload = init
